ed943057f2c0de9835e651a2382e9d30f3a0930d
nillbot <163786108+nillbot@users.noreply.github.com>
2024-04-01 05:51:43 +0000

    Update logger.py

--- Scrapyt/logger.py
+++ Scrapyt/logger.py
@@ -2,11 +2,15 @@
 
 import logging
 
-def _setup_logger():
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-    return logger
+class logger:
+    def __init__(self) -> None:
+        pass
+
+    def _setup_logger():
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+        return logger
